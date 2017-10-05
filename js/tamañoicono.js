665c6391f7eae54b7b82730066582d2916ea9fb3
johanavmag <johana.vmaga@gmail.com>
2017-10-05 16:09:41 -0500

    Avances al 4 de octubre

---
+++ js/tamañoicono.js
@@ -0,0 +1,5 @@
+$(".left").hover(function(){
+    $('.iconbar').css("height", "4em").css("width", "4em").css("margin-left","0%").css("margin-top","3%");
+    }), $(".left").mouseover(function(){
+    $('.iconbar').css("height", "10em").css("width", "10em").css("margin-left","20%");
+});
